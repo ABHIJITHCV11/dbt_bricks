2030a8167e21d15b5a3801ebb3caee274783b52e
ABHIJITHCV11 <abhijithcvijayaraghavan@gmail.com>
2025-10-05 21:31:09 +0530

    fix date cols in silver

--- dbt_dbx_prj/models/silver/silver_product_performance.sql
+++ dbt_dbx_prj/models/silver/silver_product_performance.sql
@@ -4,13 +4,17 @@ with sales as (
 ),
 product as (
     select * from {{ ref('bronze_fact_dim_product') }}
+),
+date_dim as (
+    select * from {{ ref('bronze_dim_date') }}
 )
 select
     p.product_sk,
     p.product_name,
     round(sum(s.quantity), 2) as total_quantity_sold,
     round(sum(s.net_amount), 2) as total_revenue,
-    max(s.date_sk) as last_purchase_date
+    max(d.date) as last_purchase_date
 from product p
 left join sales s on p.product_sk = s.product_sk
+left join date_dim d on s.date_sk = d.date_sk
 group by p.product_sk, p.product_name
